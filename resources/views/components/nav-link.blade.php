6f98801474e69c7bc999915e8a50b83a97918696
Robindebie <335833@student.mboutrecht.nl>
2024-09-30 11:37:46 +0200

    5de les

--- resources/views/components/nav-link.blade.php
+++ resources/views/components/nav-link.blade.php
@@ -1,4 +1,4 @@
-@props(['active'])
+@props(['active' => false ])
 
 <a class="{{ $active ? 'bg-gray-900 text-white':'text-gray-300 hover:bg-gray-700 hover:text-white' }} px-3 py-2 text-sm font-medium text-white" 
 aria-current="{{ $active ? 'page':'false' }}"
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,8 +1,6 @@
 <x-layout>
-    
     <x-slot:heading>
-        Home page
-    </x-slot:heading>
-
-
-</x-layout>
+            Home page
+        </x-slot:heading>
+    <h1>hello from the Home page</h1>
+    </x-layout>
