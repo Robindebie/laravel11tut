0c5395e9799e9e19bf14f52d6c09860d8e6d0b05
Robindebie <335833@student.mboutrecht.nl>
2024-10-07 11:45:48 +0200

    8de les

--- routes/web.php
+++ routes/web.php
@@ -1,54 +1,27 @@
 <?php
-use \Illuminate\Support\Arr;
-use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
+use Illuminate\Support\Facades\Route;
+use App\Models\job;
+
+
+
+
+
+Route::get('/', function ()  {
     return view('home');
 });
 
 Route::get('/jobs', function () {
     return view('jobs', [
-        'jobs' => [
-            [
-                'id' => 1,
-                'title' => 'Director',
-                'salary' => '49.050'
-            ],
-            [
-                'id' => 2,
-                'title' => 'Programmer',
-                'salary' => '10.000'
-            ],
-            [
-                'id' => 3,
-                'title' => 'Teacher',
-                'salary' => '40.000'
-            ]
-        ]
+        'jobs' => job::all()
+         
     ]);
 });
 
-Route::get('/jobs/{id}', function ($id) {
-    $jobs = [
-        [
-            'id' => 1,
-            'title' => 'Director',
-            'salary' => '49.050'
-        ],
-        [
-            'id' => 2,
-            'title' => 'Programmer',
-            'salary' => '10.000',
-            'geheim' => 'robin is raar'
-        ],
-        [
-            'id' => 3,
-            'title' => 'Teacher',
-            'salary' => '40.000'
-        ]
-        ];
+Route::get('/jobs/{id}', function ($id)  {
+    
+      $job = job::find($id);
 
-        $job = Arr::first($jobs, fn($job) => $job['id'] == $id);
 
     return view('job', ['job' => $job]);
 });
